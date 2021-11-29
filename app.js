25133e44d46796d2ac643bdefb3c8bd87a015789
JeffG05 <Gugelmann.jeff@gmail.com>
2021-11-29 17:52:46 +0000

    Added relevance rating

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(
 );
 
 const AssistantV2 = require("ibm-watson/assistant/v2");
+const DiscoveryV2 = require("ibm-watson/discovery/v1");
 const { IamAuthenticator } = require("ibm-watson/auth");
 
 const AssistantId = "5b1b16e6-2b64-4e35-952a-bc7eb3380250"
@@ -29,19 +30,32 @@ const assistant = new AssistantV2({
     "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/8d74b58f-52d4-4fed-971f-15abc4c09151",
 });
 
-// FORMAT OUR API RESPONDS WITH
-class APIResponse {
-    header;
-    text;
-    confidence = 1;
+const DiscoveryEnvironmentId = "2ab12c80-05d8-4232-9a93-a2a85d6c642e"
+const DiscoveryCollectionId = "592bc518-0f0d-4a76-9ba9-d5a1f5536b6a"
+const discovery = new DiscoveryV2({
+  version: "2020-08-30",
+  authenticator: new IamAuthenticator({
+    apikey: "CdIbNZTEHeoctQJZs8NgfDkM1dTfwkZ4AsHGpTpQXfrv",
+  }),
+  serviceUrl:
+    "https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/0827ccfe-7eb7-4648-b371-a836867b4ba5",
+})
 
-    json() {
-        return {
-            header: this.header,
-            text: this.text,
-            confidence: this.confidence
-        }
+// Class used to send data back to frontend
+class APIResponse {
+  documentId;
+  header;
+  text;
+  confidence = 1;
+
+  json() {
+    return {
+      id: this.documentId,
+      header: this.header,
+      text: this.text,
+      confidence: this.confidence
     }
+  }
 }
 
 app.on('listening', () => {
@@ -65,44 +79,41 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/createsession", (req, res) => {
-  assistant
-    .createSession({
+app.post("/createsession", async (req, res) => {
+
+  try {
+    let response = await assistant.createSession({
       assistantId: AssistantId,
     })
-    .then((response) => {
-      let sessionId = response.result.session_id;
-      res.status(200).json({
-        session_id: sessionId,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err);
+    let sessionId = response.result.session_id;
+    res.status(200).json({
+      session_id: sessionId,
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
-app.post("/deletesession", (req, res) => {
+app.post("/deletesession", async (req, res) => {
   const sessionId = req.body.sessionId;
   if (sessionId == null) {
     res.status(500).send("Session ID is missing");
   }
 
-  assistant
-    .deleteSession({
+  try {
+    await assistant.deleteSession({
       assistantId: AssistantId,
       sessionId: sessionId,
     })
-    .then((response) => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err);
-    });
+    res.sendStatus(200);
+  } catch(err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
-app.post("/send", (req, res) => {
+app.post("/send", async (req, res) => {
   const sessionId = req.body.sessionId;
   if (sessionId == null) {
     res.status(500).send("Session Id is missing");
@@ -115,26 +126,47 @@ app.post("/send", (req, res) => {
     return;
   }
 
-  assistant
-    .message({
-      assistantId: AssistantId,
-      sessionId: sessionId,
-      input: {
-        message_type: "text",
-        text: message,
-      },
-    })
-    .then((response) => {
-      const answers = response.result.output.generic ?? [];
-      const parsed = extractAnswers(answers);
-
-      res.status(200).json(parsed);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err);
-    });
+  try {
+    let answers = await getAnswersToQuery(sessionId, message)
+    res.status(200).json(answers)
+  } catch(err) {
+    console.error(err)
+    res.status(500).send(err)
+  }
 });
+
+app.post("/rate", async (req, res) => {
+  let query = req.body.query
+  if (query == null) {
+    res.status(500).send("Query is missing");
+    return;
+  }
+  query = query.toLowerCase().trim()
+
+  const documentId = req.body.documentId
+  if (documentId == null) {
+    res.status(500).send("Document Id is missing");
+    return;
+  }
+
+  const isRelevant = req.body.relevant
+  if (isRelevant == null) {
+    res.status(500).send("Is Relevant is missing");
+    return;
+  }
+
+  try {
+    let existingQueryId = await getTrainingQueryId(query)
+    if (existingQueryId) {
+      addExampleToTrainingQuery(existingQueryId, documentId, isRelevant)
+    } else {
+      addNewTrainingQuery(query, documentId, isRelevant)
+    }
+    res.sendStatus(200)
+  } catch(err) {
+    res.status(500).send(err)
+  }
+})
 
 app.post("/authenticate", (req, res) => {
   const username = req.body.username;
@@ -158,8 +190,136 @@ app.post("/authenticate", (req, res) => {
   }
 })
 
+// Convert to relevance scale
+function getRelevance(isRelevant) {
+  return isRelevant ? 10 : 0
+}
 
+// List all training queries
+async function getTrainingQueries() {
+  try {
+    let trainingDataSet = await discovery.listTrainingData({
+      environmentId: DiscoveryEnvironmentId,
+      collectionId: DiscoveryCollectionId
+    })
+    return trainingDataSet.result.queries ?? []
+  } catch (err) {
+    throw err
+  }
+}
+
+// Get query_id of a query (given that it exists)
+async function getTrainingQueryId(query) {
+  try {
+    let trainingQueries = await getTrainingQueries()
+    let filteredQueries = trainingQueries.filter(trainingQuery => {
+      return trainingQuery.natural_language_query == query.toLowerCase().trim()
+    })
+    if (filteredQueries.length == 0) {
+      return null
+    } else {
+      return filteredQueries[0].query_id
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+// Add new training query
+async function addNewTrainingQuery(query, documentId, isRelevant) {
+  try {
+    let trainingQuery = await discovery.addTrainingData({
+      environmentId: DiscoveryEnvironmentId,
+      collectionId: DiscoveryCollectionId,
+      naturalLanguageQuery: query,
+      examples: [{
+        document_id: documentId,
+        relevance: getRelevance(isRelevant)
+      }]
+    })
+    return trainingQuery.result.query_id
+  } catch (err) {
+    throw err
+  }
+}
+
+// Add new example to existing training query
+async function addExampleToTrainingQuery(queryId, documentId, isRelevant) {
+  try {
+    await discovery.createTrainingExample({
+      environmentId: DiscoveryEnvironmentId,
+      collectionId: DiscoveryCollectionId,
+      queryId: queryId,
+      documentId: documentId,
+      relevance: getRelevance(isRelevant)
+    })
+  } catch (err) {
+    throw err
+  }
+}
+
+// Answer a question using Watson Assistant
+async function getAnswersToQuery(sessionId, query) {
+  try {
+    let response = await assistant.message({
+      assistantId: AssistantId,
+      sessionId: sessionId,
+      input: {
+        message_type: "text",
+        text: query,
+      },
+    })
+    let rawAnswers = response.result.output.generic ?? [];
+    let extractedAnswers = extractAnswers(rawAnswers)
+    return extractedAnswers
+  } catch (err) {
+    throw err
+  }
+}
+
+// Convert answers to APIResponse classes
 function extractAnswers(answers) {
+
+  // Convert text answer to APIReponse class
+  function parseTextAnswer(answer) {
+    let apiResponse = new APIResponse()
+    apiResponse.text = answer.text
+    return apiResponse.json()
+  }
+
+  // Convert search answer to APIReponse class
+  function parseSearchAnswer(answer) {
+  let results = [];
+
+  if (answer.primary_results.length == 0) {
+    let apiResponse = new APIResponse()
+    apiResponse.text = answer.header
+    return [apiResponse.json()]
+  }
+
+  for (let result of [...answer.primary_results, ...answer.additional_results]) {
+    let apiResponse = new APIResponse()
+    apiResponse.documentId = result.id;
+    apiResponse.header = result.title
+    apiResponse.text = result.body
+    apiResponse.confidence = result.result_metadata.confidence
+    results.push(apiResponse.json());
+  }
+
+  results.sort((a, b) => {
+    return b.confidence - a.confidence;
+  });
+
+  const MAX_RESULTS = 5
+  if (results.length == 0) {
+    return null;
+  } else if (results.length > MAX_RESULTS) {
+    return results.slice(0, MAX_RESULTS)
+  } else {
+      return results
+  }
+  }
+
   let parsed = [];
   for (let answer of answers) {
     switch (answer.response_type) {
@@ -181,43 +341,6 @@ function extractAnswers(answers) {
   }
 
   return parsed;
-}
-
-function parseTextAnswer(answer) {
-    let apiResponse = new APIResponse()
-    apiResponse.text = answer.text
-    return apiResponse.json()
-}
-
-function parseSearchAnswer(answer) {
-  let results = [];
-
-  if (answer.primary_results.length == 0) {
-    let apiResponse = new APIResponse()
-    apiResponse.text = answer.header
-    return [apiResponse.json()]
-  }
-
-  for (let result of [...answer.primary_results, ...answer.additional_results]) {
-    let apiResponse = new APIResponse()
-    apiResponse.header = result.title
-    apiResponse.text = result.body
-    apiResponse.confidence = result.result_metadata.confidence
-    results.push(apiResponse.json());
-  }
-  
-  results.sort((a, b) => {
-    return b.confidence - a.confidence;
-  });
-
-  const MAX_RESULTS = 5
-  if (results.length == 0) {
-    return null;
-  } else if (results.length > MAX_RESULTS) {
-    return results.slice(0, MAX_RESULTS)
-  } else {
-      return results
-  }
 }
 
 module.exports = app;
