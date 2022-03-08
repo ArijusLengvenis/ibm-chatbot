21af2adb446881f5b7f809a7d00672c41d050524
JeffG05 <Gugelmann.jeff@gmail.com>
2022-03-08 12:13:44 +0000

    renamed out folder to docs

--- jsdoc-formatter.js
+++ jsdoc-formatter.js
@@ -12,66 +12,66 @@ function updateHtml(html) {
     return html
 }
 
-let html = fs.readFileSync("out/code/index.html").toString()
+let html = fs.readFileSync("docs/code/index.html").toString()
 html = html.replace(
     "<title>JSDoc: Home</title>",
     "<title>Code Documentation</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/index.html", html)
+fs.writeFileSync("docs/code/index.html", html)
 
-html = fs.readFileSync("out/code/module-app.html").toString()
+html = fs.readFileSync("docs/code/module-app.html").toString()
 html = html.replace(
     "<title>JSDoc: Module: app</title>",
     "<title>Code Documentation - app.js</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/module-app.html", html)
+fs.writeFileSync("docs/code/module-app.html", html)
 
-html = fs.readFileSync("out/code/module-updater.html").toString()
+html = fs.readFileSync("docs/code/module-updater.html").toString()
 html = html.replace(
     "<title>JSDoc: Module: updater</title>",
     "<title>Code Documentation - updater.js</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/module-updater.html", html)
+fs.writeFileSync("docs/code/module-updater.html", html)
 
-html = fs.readFileSync("out/code/module-uploader.html").toString()
+html = fs.readFileSync("docs/code/module-uploader.html").toString()
 html = html.replace(
     "<title>JSDoc: Module: uploader</title>",
     "<title>Code Documentation - uploader.js</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/module-uploader.html", html)
+fs.writeFileSync("docs/code/module-uploader.html", html)
 
-html = fs.readFileSync("out/code/module-app.APIResponse.html").toString()
+html = fs.readFileSync("docs/code/module-app.APIResponse.html").toString()
 html = html.replace(
     "<title>JSDoc: Class: APIResponse</title>",
     "<title>Code Documentation - APIResponse</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/module-app.APIResponse.html", html)
+fs.writeFileSync("docs/code/module-app.APIResponse.html", html)
 
-html = fs.readFileSync("out/code/app.js.html").toString()
+html = fs.readFileSync("docs/code/app.js.html").toString()
 html = html.replace(
     "<title>JSDoc: Source: app.js</title>",
     "<title>Code Documentation - Source: app.js</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/app.js.html", html)
+fs.writeFileSync("docs/code/app.js.html", html)
 
-html = fs.readFileSync("out/code/src_js_updater.js.html").toString()
+html = fs.readFileSync("docs/code/src_js_updater.js.html").toString()
 html = html.replace(
     "<title>JSDoc: Source: src/js/updater.js</title>",
     "<title>Code Documentation - Source: updater.js</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/src_js_updater.js.html", html)
+fs.writeFileSync("docs/code/src_js_updater.js.html", html)
 
-html = fs.readFileSync("out/code/src_js_uploader.js.html").toString()
+html = fs.readFileSync("docs/code/src_js_uploader.js.html").toString()
 html = html.replace(
     "<title>JSDoc: Source: src/js/uploader.js</title>",
     "<title>Code Documentation - Source: uploader.js</title>"
 )
 html = updateHtml(html)
-fs.writeFileSync("out/code/src_js_uploader.js.html", html)
+fs.writeFileSync("docs/code/src_js_uploader.js.html", html)
